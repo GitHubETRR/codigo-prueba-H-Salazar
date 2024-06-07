a438470f979817e7658340d7d6b3e75b6754e7dd
H-Salazar <hsalazar@etrr.edu.ar>
2024-06-07 17:35:02 +0000

    Ejercicio 14
    Construye un programa que permita ingresar los valores de 2 de los ángulos interiores de un triángulo, y se emita por pantalla el valor del restante

--- calculo de area de figuras.c
+++ calculo de area de figuras.c
@@ -3,32 +3,33 @@ const int PI=3.14;
 
 int main(void)
 {
-    int area, figura, radio, largo, ancho;
+    int Area, Figura, Radio, Largo, Ancho, Perimetro;
     printf("1 para rectangulo/cuadrado, cualquier otro numero circunferencia: ");
-    scanf("%d", &figura);
-    if (figura==1)
-    {
+    scanf("%d", &Figura);
+    if (Figura==1){
         printf("Ingrese largo del rectangulo o cuadrado: ");
-        scanf("%d", &largo);
+        scanf("%d", &Largo);
         printf("Ingrese ancho del rectangulo o cuadrado: ");
-        scanf("%d", &ancho);
-        area=largo*ancho;
-        if (largo==ancho)
-        {
-        printf("El area del cuadrado vale: %d", area);
+        scanf("%d", &Ancho);
+        Area=Largo*Ancho;
+        if (Largo==Ancho){
+            printf("El area del cuadrado vale: %d\n", Area);
+            Perimetro=4*Largo;
+            printf("El perimetro del cuadrado vale: %d\n", Perimetro);
         }
-        else
-        {
-        printf("El area del rectangulo vale: %d", area);
+        else{
+            Perimetro=2*(Largo+Ancho);
+            printf("El area del rectangulo vale: %d\n", Area);
+            printf("El perimetro del rectangulo vale: %d\n", Perimetro);
         }
     }
-    else
-    {
+    else{
         printf("Radio de la circunferencia: ");
-        scanf("%d", &radio);
-        area=PI*radio*radio;
-        printf("El area de la circunferencia vale: %d", area);
+        scanf("%d", &Radio);
+        Area=PI*Radio*Radio;
+        Perimetro=2*PI*Radio;
+        printf("El area de la circunferencia vale: %d\n", Area);
+        printf("El perimetro de la circunferencia vale: %d", Perimetro);
     }
-
     return 0;
 }
