bd640ee5572a01bf38ae0b81009c642a153717ac
H-Salazar <hsalazar@etrr.edu.ar>
2024-08-09 17:56:55 +0000

    Ejercicio 1 y 2 del PP de vectores

---
+++ Ejercicio 1 y 2 vectores.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#define MAX 5
+int SumaVector(int []);
+double PromedioVector(double);
+CargarVector(int []);
+
+int main() {
+  int vector[MAX] = {0};
+  CargarVector(vector);
+  int Suma=SumaVector(vector);
+  double Pro=PromedioVector(Suma);
+  printf("La suma de los numeros del vector es: %d\n", Suma);
+  printf("El promedio de los numero del vector es: %.2f", Pro);
+  return 0;
+}
+
+CargarVector(int vector[]){
+  for (int i=0; i<MAX; i++){
+    printf("\nIngrese un numero %d de %d: ", i+1,MAX);
+    scanf("%d", &vector[i]);
+  }
+}
+
+int SumaVector(int vector[]) {
+  int Suma=0;
+  for (int i=0;i<MAX;i++) Suma+=vector[i];
+  return Suma;
+}
+
+double PromedioVector(double Suma){
+  double Promedio=Suma/MAX;
+  return Promedio;
+}
